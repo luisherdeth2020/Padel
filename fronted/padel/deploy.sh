38a8d64e4979f0d2d52d3a5b0297b7e8d3f40d33
Luis Herdeth <luis.herdeth@gmail.com>
2023-01-30 21:12:13 +0100

    rename deploy2

--- fronted/padel/deploy.sh
+++ fronted/padel/deploy.sh
@@ -24,6 +24,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:luisherdeth2020/Padel/tree/main/fronted/padel.git main:gh-pages
+# git push -f git@github.com:luisherdeth2020/padel.git main:gh-pages
 
 cd -
